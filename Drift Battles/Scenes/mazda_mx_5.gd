93019f0b9482ab07fa0d7c840280477b9b999b1d
EarlWSC <ocdaoch@wsc.school.nz>
2024-08-20 12:27:50 +1200

    CHASE CAMERA SORTA WORKS
    Yay

--- Drift Battles/Scenes/mazda_mx_5.gd
+++ Drift Battles/Scenes/mazda_mx_5.gd
@@ -4,6 +4,7 @@ var max_rpm = 600
 var max_torque = 200
 
 func _physics_process(delta):
+	
 	steering = lerp(steering, Input.get_axis("right","left") * 0.4, 5 * delta)
 	var acceleration = Input.get_axis("back","forward") * 100
 	var rpm = abs($LR_Wheel.get_rpm())

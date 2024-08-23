8a304fda33dc19dfa03c9b12e414b1b7ce2c7a83
EarlWSC <ocdaoch@wsc.school.nz>
2024-08-23 19:23:19 +1200

    handbrake with good friction slip

--- Drift Battles/Scenes/mazda_mx_5.gd
+++ Drift Battles/Scenes/mazda_mx_5.gd
@@ -1,14 +1,19 @@
 extends VehicleBody3D
 
-var max_wheelspin = 600
-var max_torque = 151
-var handbrake_force = 2000
+var max_wheelspin = 800
+var max_torque = 251
+var handbrake_force = 8000
 var handbrake_applied = false
+var original_rear_friction = 1
+
+func _ready():
+	# Store the original friction slip of the rear tires
+	original_rear_friction = $LR_Wheel.wheel_friction_slip
 
 func _physics_process(delta):
-	
-	steering = lerp(steering, Input.get_axis("right", "left") * 0.6, 11 * delta)
-	var acceleration = Input.get_axis("back", "forward") * 500
+# First # Value is Steering Range and Second # Value is Speed of steering from left to right/ vice versa
+	steering = lerp(steering, Input.get_axis("right", "left") * 1 , 11 * delta)
+	var acceleration = Input.get_axis("back", "forward") * 800 #Horsepower
 	var wheelspin = abs($LR_Wheel.get_rpm())
 	$LR_Wheel.engine_force = acceleration * max_torque * (1 - wheelspin / max_wheelspin)
 	wheelspin = abs($RR_Wheel.get_rpm())
@@ -23,10 +28,14 @@ func _physics_process(delta):
 func apply_handbrake(apply: bool):
 	handbrake_applied = apply
 	if handbrake_applied:
-		# Apply handbrake force to the rear wheels
+		# Apply handbrake force to the rear wheels and reduce friction for sliding
 		$LR_Wheel.brake = handbrake_force
 		$RR_Wheel.brake = handbrake_force
+		$LR_Wheel.wheel_friction_slip = 0.2 # Lower friction slip to allow drifting
+		$RR_Wheel.wheel_friction_slip = 0.2
 	else:
-		# Release the handbrake force
+		# Release the handbrake force and restore original friction
 		$LR_Wheel.brake = 0
 		$RR_Wheel.brake = 0
+		$LR_Wheel.wheel_friction_slip = original_rear_friction
+		$RR_Wheel.wheel_friction_slip = original_rear_friction

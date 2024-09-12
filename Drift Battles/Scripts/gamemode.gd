e34c34739f25fe4cf561bcaff6cc9420ce7f25af
EarlWSC <ocdaoch@wsc.school.nz>
2024-09-12 12:26:17 +1200

    IM GETTING GOOD PROGRESS
    woohoo its actually going great

---
+++ Drift Battles/Scripts/gamemode.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_race_day_pressed():
+	pass
+
+func _on_practice_session_pressed():
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+func _on_time_trials_pressed():
+	pass 

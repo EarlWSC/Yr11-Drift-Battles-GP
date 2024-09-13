5a4857ca2981ac572fe063626805707890f36fc9
EarlWSC <ocdaoch@wsc.school.nz>
2024-09-13 12:57:02 +1200

    Sprint 2 Completed, but my tasks aren't!

--- Drift Battles/Scripts/gamemode.gd
+++ Drift Battles/Scripts/gamemode.gd
@@ -4,7 +4,10 @@ func _on_race_day_pressed():
 	pass
 
 func _on_practice_session_pressed():
-	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+	get_tree().change_scene_to_file("res://Scenes/World.tscn")
 
 func _on_time_trials_pressed():
 	pass 
+	
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")

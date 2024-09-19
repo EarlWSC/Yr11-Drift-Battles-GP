a34dadbf13e5be67e7c1e1144d9cb01d7fa5670b
EarlWSC <ocdaoch@wsc.school.nz>
2024-09-19 12:29:38 +1200

    3/4 tracks ready

--- Drift Battles/Scripts/circuit_select.gd
+++ Drift Battles/Scripts/circuit_select.gd
@@ -1,18 +1,21 @@
 extends Control
 
-func _on_silverstone_pressed():
+func _on_tsukuba_pressed():
 	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 
-func _on_interlagos_pressed():
-	pass
+func _on_interlagos_pressed(): #THIS IS ACTUALLY JEREZ CIRCUIT
+	get_tree().change_scene_to_file("res://Scenes/jerez.tscn")
 
 
-func _on_spafrancorchamps_pressed():
-	pass 
+func _on_spafrancorchamps_pressed(): #THIS IS ACTUALLY ARGENTINA CIRCUIT
+	get_tree().change_scene_to_file("res://Scenes/argentina.tscn")
 
 
-func _on_bahrain_pressed():
-	pass 
+func _on_bahrain_pressed(): #THIS IS ACTUALLY SPLIT TRACK CIRCUIT
+	if global.gamemode_select == 2:
+		get_tree().change_scene_to_file("res//Scenes/")
+	else:
+		print("You ain't allowed here if you are trying to race or time trial buddy o pal!")
 	
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/gamemode.tscn")

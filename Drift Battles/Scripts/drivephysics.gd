dbf43ffd58857d3f0503c1156b828af761966549
EarlWSC <ocdaoch@wsc.school.nz>
2024-09-24 12:27:41 +1200

    gear shifting on the works

--- Drift Battles/Scripts/drivephysics.gd
+++ Drift Battles/Scripts/drivephysics.gd
@@ -1,13 +1,41 @@
 extends VehicleBody3D
 
 var max_wheelspin = 1000
-var max_torque = 400
+var torque = 400
+var gear = 0
+var acceleration = 0
 
 func _physics_process(delta):
+	print($LR_Wheel.wheel_friction_slip)
 	steering = lerp(steering, Input.get_axis("right","left") * 0.2, 5 * delta)
-	var acceleration = Input.get_axis("back","forward") * 40
+	if Input.is_action_just_pressed("gearup"):
+		gear = gear + 1
+		if gear > 5:
+			gear = gear - 1
+		print(gear)
+	if Input.is_action_just_pressed("geardown"):
+		gear = gear - 1
+		if gear < -1:
+			gear = gear + 1
+		print(gear)
+	if Input.is_action_pressed("forward"):
+		acceleration = (20 * gear)
+		
+		
+	else:
+		acceleration = 0
+	if Input.is_action_pressed("back"):
+		$LF_Wheel.brake = 20
+		$RF_Wheel.brake = 20
+		$LR_Wheel.brake = 40
+		$RR_Wheel.brake = 40
+	else:
+		$LF_Wheel.brake = 0
+		$RF_Wheel.brake = 0
+		$LR_Wheel.brake = 0
+		$RR_Wheel.brake = 0
 	var wheelspin = abs($LR_Wheel.get_rpm())
-	$LR_Wheel.engine_force = acceleration * max_torque * (1 - wheelspin / max_wheelspin)
+	$LR_Wheel.engine_force = acceleration * torque * (1 - wheelspin / max_wheelspin)
 	wheelspin = abs($RR_Wheel.get_rpm())
-	$RR_Wheel.engine_force = acceleration * max_torque * (1 - wheelspin / max_wheelspin)
+	$RR_Wheel.engine_force = acceleration * torque * (1 - wheelspin / max_wheelspin)
 	

6d56e010dc077a900a243b6c80b440a263b40c8a
EarlWSC <ocdaoch@wsc.school.nz>
2024-09-26 12:27:54 +1200

    I DONT GET IT :(

--- Drift Battles/Scripts/drivephysics.gd
+++ Drift Battles/Scripts/drivephysics.gd
@@ -1,41 +1,46 @@
 extends VehicleBody3D
 
 var max_wheelspin = 1000
-var torque = 400
-var gear = 0
+var torque = 100
+var gear_ratio = [0.1, 3.5, 2.1, 1.4, 1.0, 0.8]
+var current_gear = 0
 var acceleration = 0
-
-func _physics_process(delta):
-	print($LR_Wheel.wheel_friction_slip)
-	steering = lerp(steering, Input.get_axis("right","left") * 0.2, 5 * delta)
+var speed = 0
+func _process(delta):
+	#MANUAL GEAR SWITCH CODE
 	if Input.is_action_just_pressed("gearup"):
-		gear = gear + 1
-		if gear > 5:
-			gear = gear - 1
-		print(gear)
+		current_gear = current_gear + 1
+		if current_gear > 5:
+			current_gear = current_gear - 1
+		print(current_gear)
 	if Input.is_action_just_pressed("geardown"):
-		gear = gear - 1
-		if gear < -1:
-			gear = gear + 1
-		print(gear)
+		current_gear = current_gear - 1
+		if current_gear < -1:
+			current_gear = current_gear + 1
+		print(current_gear)
+	
+	var velocity = linear_velocity  # Get the velocity vector (Vector3)
+	speed = velocity.length()  # Calculate the magnitude of the velocity vector (speed)
+	
+func _physics_process(delta):
+	steering = lerp(steering, Input.get_axis("right","left") * 0.2, 5 * delta)
+	#THROTTLE AND BRAKE CODE
 	if Input.is_action_pressed("forward"):
-		acceleration = (20 * gear)
-		
-		
+		acceleration = (20 * current_gear)
 	else:
-		acceleration = 0
+		acceleration = 0.1
 	if Input.is_action_pressed("back"):
-		$LF_Wheel.brake = 20
-		$RF_Wheel.brake = 20
-		$LR_Wheel.brake = 40
-		$RR_Wheel.brake = 40
+		$LF_Wheel.brake = 40
+		$RF_Wheel.brake = 40
+		$LR_Wheel.brake = 80
+		$RR_Wheel.brake = 80
 	else:
 		$LF_Wheel.brake = 0
 		$RF_Wheel.brake = 0
 		$LR_Wheel.brake = 0
 		$RR_Wheel.brake = 0
 	var wheelspin = abs($LR_Wheel.get_rpm())
-	$LR_Wheel.engine_force = acceleration * torque * (1 - wheelspin / max_wheelspin)
+	$LR_Wheel.engine_force = acceleration * (torque * gear_ratio[current_gear] - (speed - current_gear)) * (1 - wheelspin / max_wheelspin) 
 	wheelspin = abs($RR_Wheel.get_rpm())
-	$RR_Wheel.engine_force = acceleration * torque * (1 - wheelspin / max_wheelspin)
+	$RR_Wheel.engine_force = acceleration * (torque * gear_ratio[current_gear] - (speed- current_gear)) * (1 - wheelspin / max_wheelspin) 
 	
